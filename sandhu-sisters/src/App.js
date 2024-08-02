68cb63feb726a29169b79c0a49eb06acf74cfd7c
Jitu gandhare <jitugandhare@gmail.com>
2024-08-02 17:48:58 +0530

    Footer section is completed

--- sandhu-sisters/src/App.js
+++ sandhu-sisters/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import AllRoutes from "./components/AllRoutes";
 import { Router } from "react-router-dom";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
      <AllRoutes/>
+     <Footer/>
     </div>
 
 
--- sandhu-sisters/src/pages/Home.jsx
+++ sandhu-sisters/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div>
       <CustomCarousel/>
       <br />
-      <CardCarousel/>
+      {/* <CardCarousel/> */}
     </div>
   )
 }
